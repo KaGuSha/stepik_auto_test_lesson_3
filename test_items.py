4dcc1806aa33ae523736532de8a1f7569da31ea1
KaGuSha <gulnarik-89@yandex.ru>
2022-05-12 10:25:37 +0200

    обновление test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,8 @@
 link = "http://selenium1py.pythonanywhere.com/de/catalogue/coders-at-work_207/"
 
 def test_is_button_add_to_basket_exist(browser):
-    browser.get(link)
-    browser.find_element_by_css_selector(".add-to-basket .btn")
-
+    try:
+        browser.get(link)
+        browser.find_element_by_css_selector(".add-to-basket .btn")
+    finally:
+        assert False, f"Button Add-to-basket is not exist and visible"
